64a7fa83a25943ec100a64cdfa683fc37d0154f5
intp5 <50936098+intp5@users.noreply.github.com>
2019-05-24 14:28:53 +0900

    Update footer.inc.php

--- include/client/footer.inc.php
+++ include/client/footer.inc.php
@@ -1,7 +1,7 @@
         </div>
     </div>
     <div id="footer">
-        <p>Copyright &copy; <?php echo date('Y'); ?> <?php echo (string) $ost->company ?: 'osTicket.com'; ?> - All rights reserved.</p>
+        <p>Copyright &copy; <?php echo date('Y'); ?> <?php echo (string) $ost->company ?: 'osTicket.com'; ?> - All rights reserved. Run on Azure App Services!</p>
         <a id="poweredBy" href="http://osticket.com" target="_blank"><?php echo __('Helpdesk software - powered by osTicket'); ?></a>
     </div>
 <div id="overlay"></div>
